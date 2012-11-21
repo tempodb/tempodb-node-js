dcc05cb80084682ac7d43f43e49bff3b41c7c92b
Patrick Madden <maddenpj@gmail.com>
2012-11-21 13:10:32 -0600

    Added Delete{key,id}

---
+++ demo/tempodb-delete-demo.js
@@ -0,0 +1,15 @@
+
+var TempoDBClient = require('tempodb').TempoDBClient;
+var tempodb = new TempoDBClient('your-api-key', 'your-api-secret');
+
+var series_key = 'your-custom-key'
+// Delete a day's worth of data
+series_start_date = new Date('2012-11-13')
+series_end_date = new Date('2012-11-14')
+
+var start_time = new Date();
+tempodb.delete_key(series_key, series_start_date, series_end_date, function(result){
+  console.log(result.response + ': ' + JSON.stringify(result.body));
+  console.log('Completed in', new Date() - start_time, 'ms\n');
+
+});
