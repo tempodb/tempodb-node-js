bc41798b8b12032da43579ea50fcd999242eaaa8
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2014-02-04 13:03:57 -0600

    working read demo and syntax fixes

--- demo/tempodb-read-demo.js
+++ demo/tempodb-read-demo.js
@@ -3,7 +3,7 @@
 var TempoDBClient = require('../lib/tempodb').TempoDBClient;
 var tempodb = new TempoDBClient('my-key', 'my-secret');
 
-var series_key = 'my-series',
+var series_key = 'stuff',
 	series_start_date = new Date('2012-01-01'),
 	series_end_date = new Date('2012-01-02');
 
@@ -18,27 +18,16 @@ var count = 0
 var start_time = new Date();
 tempodb.read(series_key, series_start_date, series_end_date, options, function(err, result){
 	if (err) {
-		//console.log('Response code: ' + result.status);
-		//console.log('Error reason: ' + result.json);
 		console.log(err);
 		console.log('Status code: ' + err.status);
 		console.log('Error: ' + err.json);
 	} else {
-		//console.log('Response: ' + JSON.stringify(result.json));
-		//console.log('Number of items in response: ' + result.json.data.length);
-		result.json.data.map(function(err, dps) {
+		result.json.data.readAll(function(err, dps) {
 			if (err) { 
-				//console.log('Error: ' + err);
 				console.log('There was an error')
-				//console.log(dps);
 			} else {
-				//for (var i=0; i<dps.length; i++) {
-					//console.log('Datapoint: (t: ' + dps[i].t + ' v: ' + dps[i].v + ')');
-				//}
 				console.log('Total points: ' + dps.length);
 			}
 		});
-		//console.log('Total datapoints: ' + count);
-		//console.log('Completed in ', new Date() - start_time, 'ms\n');
 	}
 });
