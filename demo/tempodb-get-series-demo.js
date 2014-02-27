7b38e36673e35109433a0e7a4e865454bb9b2645
Aaron Brenzel <aaron.brenzel@tempo-db.com>
2014-02-27 14:52:28 -0600

    demo for get series

---
+++ demo/tempodb-get-series-demo.js
@@ -0,0 +1,22 @@
+/* http://tempo-db.com/api/read-series/#read-series-by-key */
+
+var TempoDBClient = require('../lib/tempodb').TempoDBClient;
+var tempodb = new TempoDBClient('my-key', 'my-secret')
+
+// supply your own options here
+var options = {}
+var count = 0
+
+var start_time = new Date();
+tempodb.getSeries(options, function(err, result){
+	if (err) {
+		console.log(err);
+		console.log('Status code: ' + err.status);
+		console.log('Error: ' + err.json);
+	} else {
+    result.json.data.toArray(function(err, result) {
+		  console.log(result)
+		  console.log('Total series: ' + result.length);
+    })
+	}
+});
